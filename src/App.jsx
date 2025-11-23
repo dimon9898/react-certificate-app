08dc4db39d95954af6f35454a770f6711a6e6f96
Дима <test1@MacBook-Air-test.local>
2025-11-23 12:15:30 +0300

    update webapp

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   }, []);
 
   const BuyCertificate = async (certifi) => {
-    const tg = window.Telegram.WebApp();
+    const tg = window.Telegram.WebApp;
     tg.sendData(JSON.stringify(certifi));
   };
 
