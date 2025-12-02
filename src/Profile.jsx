0d5b638f8b33068b593b5835b21c706c5e2a7376
Дима <test1@MacBook-Air-test.local>
2025-12-02 16:45:41 +0300

    update header100

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -17,9 +17,8 @@ export default function ProfileCard({userInfo, isAccount, hideProfile}) {
         <div className={`ProfilContainer ${isAccount ? 'open_profile' : ''}`}>
             <div className="profile-box">
                 <div className="header-top">
-                    <p>ID: {user?.id}</p>
-                    <p>Имя: {user?.first_name}</p>
-                    <p>Тел:</p>
+                    <p>ID: <code>{user?.id}</code></p>
+                    <p>Имя: <code>{user?.first_name}</code></p>
                     <button className='profile-close-btn' onClick={hideProfile}><FontAwesomeIcon icon={faCircleXmark} /></button>
                 </div>
             </div>
