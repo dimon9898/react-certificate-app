cfa315afb7b0c4d76c238d51fc4cd01a68d85974
Дима <test1@MacBook-Air-test.local>
2025-12-02 16:16:04 +0300

    update header99

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react';
 
 export default function ProfileCard({userInfo, isAccount, hideProfile}) {
-    window?.Telegram?.WebApp?.ready();
-    const user = window?.Telegram?.WebApp?.initDataUnsafe?.user;
+    const [user, setUser] = useState(null);
+
+
+    useEffect(() => {
+        const tg = window?.Telegram?.WebApp;
+        tg?.ready();
+        setUser(tg?.initDataUnsafe?.user || null);
+    }, []);
 
     return (
         <div className={`ProfilContainer ${isAccount ? 'open_profile' : ''}`}>
