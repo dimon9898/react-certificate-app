7458a7206214c0bc33fb67d502763877cdabb1f7
Дима <test1@MacBook-Air-test.local>
2025-12-04 21:59:20 +0300

    update user-info

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import { faAnglesRight, faCircleInfo, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import { faCartFlatbed } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 
 export default function ProfileCard({userInfo, isAccount, hideProfile}) {
@@ -17,9 +18,26 @@ export default function ProfileCard({userInfo, isAccount, hideProfile}) {
         <div className={`ProfilContainer ${isAccount ? 'open_profile' : ''}`}>
             <div className="profile-box">
                 <div className="header-top">
-                    <p>ID: <code>{user?.id}</code></p>
-                    <p>Имя: <code>{user?.first_name}</code></p>
+                    <div className='profile-img-box'>
+                        <div className="profile-user-img"></div>
+                    </div>
+                    <div className="profile-user-info">
+                        <p>ID: <code>{user?.id}</code></p>
+                        <p>Имя: <code>{user?.first_name}</code></p>
+                    </div>
                     <button className='profile-close-btn' onClick={hideProfile}><FontAwesomeIcon icon={faCircleXmark} /></button>
+                </div>
+                <div className="profile-box-center">
+                    <button className='box-center-item'>
+                        <FontAwesomeIcon icon={faCartFlatbed} className='fa-icon'/>
+                        <a href="*">Мои покупки</a>
+                        <FontAwesomeIcon icon={faAnglesRight} className='fa-right-icon'/>
+                    </button>
+                    <button className='box-center-item'>
+                        <FontAwesomeIcon icon={faCircleInfo} className='fa-icon'/>
+                        <a href="*">Информация</a>
+                        <FontAwesomeIcon icon={faAnglesRight} className='fa-right-icon'/>
+                    </button>
                 </div>
             </div>
         </div>
