6c8c51d49253c7efbc45bcc254fbaedab819f1bb
Дима <test1@MacBook-Air-test.local>
2025-12-06 14:40:38 +0300

    update

--- src/MyOrders.jsx
+++ src/MyOrders.jsx
@@ -8,17 +8,22 @@ export default function MyOrdersPage({ OpenOrders }) {
 
     const [isOpenOrders, setIsOpenOrders] = useState(false);
     const [purchases, setPurchases] = useState([]);
+    const [isLoader, setIsLoader] = useState(true);
 
     useEffect(() => {
+        setIsLoader(true);
         fetchPurchases();
         const timer = setTimeout(() => {
             setIsOpenOrders(true);
         }, 50);
+        setTimeout(() => {
+            setIsLoader(false);
+        }, 1500);
         return () => clearTimeout(timer);
     }, [])
 
     const fetchPurchases = async () => {
-        const response = await fetch('https://ds-bot-h5l3.onrender.com/api/mypurchases', {
+        const response = await fetch('http://127.0.0.1:8000/api/mypurchases', {
             method: 'GET',
             headers: {'Content-Type': 'application/json'}
         })
@@ -37,6 +42,11 @@ export default function MyOrdersPage({ OpenOrders }) {
 
     return (
         <div className={`my-orders-container ${isOpenOrders ? 'my-orders-open' : '' }`}>
+            {isLoader && (
+                <div className="purchase-loader-container">
+                    <div className="purchase-loader-box"></div>
+                </div>
+            )}
             <button className='orders-back-btn' onClick={hideOrderPage}><FontAwesomeIcon icon={faChevronLeft} /> назад</button>
             {purchases.length > 0 ? (
                 <PurchasesCard purchases={purchases}/>
